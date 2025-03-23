41304125860c50afe8526789ce887dec97a720ac
Zahra Ali <zahraali.tech@gmail.com>
2025-03-23 11:12:22 -0400

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
-};
+const sumAll  = (a,b) => {
+  let sum = 0; // declare sum
+  for (let i=a; i<=b; i++){
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
