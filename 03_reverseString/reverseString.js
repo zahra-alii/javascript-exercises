e1175e3f36b01f0485b3eb7a9ab8c50781d18c00
Zahra Ali <zahraali.tech@gmail.com>
2025-03-23 10:31:29 -0400

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+function reverseString(str){
+  return str.split('').reverse().join("");
+}
+console.log(reverseString('hello there')) // returns 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
