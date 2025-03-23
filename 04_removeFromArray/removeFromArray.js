6ff4c47a044f9cc99785f86ee629c5671e869c57
Zahra Ali <zahraali.tech@gmail.com>
2025-03-23 10:58:11 -0400

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
-
+function removeFromArray(arr, ...args){ // ... takes in limitless # of args
+  const newArr = []; // create new arr
+  arr.forEach((elem) => { // loop through each elem
+    if(!args.includes(elem)) { // if args does NOT include the element we select...
+      newArr.push(elem); // push the elem into newArr
+    }
+  });
+  return newArr;
+}
+console.log(removeFromArray([1, 2, 3, 4], 3))
 // Do not edit below this line
 module.exports = removeFromArray;
