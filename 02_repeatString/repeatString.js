f73df06d1ccfa4cbaba32c3e7005554428e8d442
Zahra Ali <zahraali.tech@gmail.com>
2025-03-23 10:28:37 -0400

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let result = ''; // decl empty str
+  for (let i = 1; i<=num; i++){
+    result += str;
+  }
+  return result;
 };
-
+console.log(repeatString('hey', 3));
 // Do not edit below this line
-module.exports = repeatString;
+// module.exports = repeatString;
+
