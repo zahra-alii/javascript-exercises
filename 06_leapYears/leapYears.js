6dffcae7e2165f8e6fea22e79758935f799f2b31
Zahra Ali <zahraali.tech@gmail.com>
2025-03-23 11:18:18 -0400

    Update leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
-};
+function leapYears(year){
+  if(year % 4 === 0 || year % 100 === 0 && year % 400 === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log(leapYears(2000)); // true
+console.log(leapYears(1985)); // false
 
 // Do not edit below this line
 module.exports = leapYears;
