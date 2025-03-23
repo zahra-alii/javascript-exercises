c0f47e829c40bfbcf455ea33fd4399401fcc85fc
Zahra Ali <zahraali.tech@gmail.com>
2025-03-23 11:30:50 -0400

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(temp){
+  let fanhrenheit = (temp - 32) * 5/9;
+  return fanhrenheit;
+}
+console.log(convertToCelsius(32));
 
-const convertToFahrenheit = function() {
-};
+
+function convertToFahrenheit(temp){
+  let celsius = (temp * 9/5) + 32;
+  return celsius;
+}
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
